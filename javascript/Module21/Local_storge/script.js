17e0fe0e2f0ca89b1f1d96bf1d007a790e82c6a4
Shami-sharma <kumarshammi5194@gamil.com>
2026-07-21 22:03:07 +0530

    change

---
+++ javascript/Module21/Local_storge/script.js
@@ -0,0 +1,8 @@
+localStorage.setItem('name','Ritik')
+localStorage.setItem('age',20)
+
+localStorage.removeItem('age');
+// localStorage.clear()
+
+sessionStorage.setItem("name","Akash");
+sessionStorage.setItem("age",35);
